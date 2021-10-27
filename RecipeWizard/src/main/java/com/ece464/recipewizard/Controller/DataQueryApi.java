8b283f0fb853efd28d3c168890df10aa64b4e868
leonnfang <fang@cooper.edu>
2021-10-27 12:55:25 -0400

    more setup

--- RecipeWizard/src/main/java/com/ece464/recipewizard/Controller/DataQueryApi.java
+++ RecipeWizard/src/main/java/com/ece464/recipewizard/Controller/DataQueryApi.java
@@ -1,8 +1,13 @@
 package com.ece464.recipewizard.Controller;
 
 
+import com.ece464.recipewizard.Model.Recipe;
+import com.ece464.recipewizard.Service.MysqlManager;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class DataQueryApi {
@@ -14,5 +19,11 @@ public class DataQueryApi {
     @RequestMapping(path = "/health")
     public boolean health(){
         return true;
+    }
+
+    @RequestMapping(path = "/search/{names}")
+    public List<Recipe> getRecipeByIngredients(@PathVariable List<String> names){
+        return MysqlManager.get(names);
+//        return null;
     }
 }
