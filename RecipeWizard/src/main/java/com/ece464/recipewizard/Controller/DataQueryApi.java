5e6730ce60592855907bd7ef0bb76cf4d469f6cc
leonfang <fang@cooper.edu>
2021-11-21 13:54:29 -0500

    comment the input and output for apis

--- RecipeWizard/src/main/java/com/ece464/recipewizard/Controller/DataQueryApi.java
+++ RecipeWizard/src/main/java/com/ece464/recipewizard/Controller/DataQueryApi.java
@@ -3,7 +3,6 @@ package com.ece464.recipewizard.Controller;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import com.ece464.recipewizard.Model.Recipe;
 import com.ece464.recipewizard.Service.RecipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -14,8 +13,11 @@ import java.util.List;
 @RestController
 public class DataQueryApi {
 
-    @Autowired
-    private RecipeRepository repository;
+    private final RecipeRepository repository;
+
+    public DataQueryApi(RecipeRepository repository) {
+        this.repository = repository;
+    }
     // query by name
     // query by ingredients
     // top recommendation ?? additional features
@@ -28,7 +30,9 @@ public class DataQueryApi {
     /**
      *
      * @param names
+     * the names users are interested in searching for
      * @return
+     * all matched recipes will be returned
      */
 
     @RequestMapping(path = "/search/recipe/{names}")
@@ -41,11 +45,15 @@ public class DataQueryApi {
     /**
      *
      * @return
+     * the list of all ingredients will be returned
      */
     @RequestMapping(path = "/info/list_ingredients")
     public JSONArray getIngredientList(){
         return new JSONArray();
     }
+
+
+
     /**
      *
      * @param names the list of ingredients
@@ -60,6 +68,7 @@ public class DataQueryApi {
     /**
      *
      * @return
+     * all recommended recipe will be returned as Json array
      */
     @RequestMapping(path = "/search/recommend")
     public JSONArray recommendRecipe(){
@@ -70,7 +79,9 @@ public class DataQueryApi {
     /**
      *
      * @param recipeInfo
+     * the recipe users are interested in loading into the database
      * @return
+     * status will be returned, to see the recipe has been updated or not
      */
     @RequestMapping(path = "/update/recipe")
     public boolean insertRecipe(JSONObject recipeInfo){
